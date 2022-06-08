0d45320ee59c42cb23b271a1d514d4b84aea5bf7
Barrera Garcia <Dbarrera@mtc.gob.pe>
2022-06-08 11:36:36 -0500

    update ejercicios.js

--- CURSO JAVASCRIP/12. ejercicio_API/ejercicio.js
+++ CURSO JAVASCRIP/12. ejercicio_API/ejercicio.js
@@ -3,15 +3,16 @@ async function imagenes(){
 const respuestas = [];
 
     for (let i=0;i<50;i++){
-        const respuesta= await fetch(`https://picsum.phothos/id/${i}/info`);
+        const respuesta= await fetch(`https://picsum.photos/id/${i}/info`);
         const imagen= await respuesta.json();
         respuestas.push(imagen);
     }
 
-
+    //console.log(i);
     console.log(respuestas);
 }
 
 imagenes();
+
 
 
